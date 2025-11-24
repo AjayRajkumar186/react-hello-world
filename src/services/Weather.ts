ad09d20306fe6fcfde28a0d63f80ce09573acc23
ajay <ajayrajkumar1702@gmail.com>
2025-11-24 20:25:27 +0530

    space-remove

--- src/services/Weather.ts
+++ src/services/Weather.ts
@@ -1,16 +1,16 @@
 import axios from "axios"
 
 const KEY = `721a52b477d842f4950153121250611`;
-  const BASE_URL = `http://api.weatherapi.com/v1`;
+const BASE_URL = `http://api.weatherapi.com/v1`;
 
 const weatherservice = {
 
-  getWeather : async(city:any) =>{
+  getWeather: async (city: any) => {
     try {
-      const response = await axios.get( `${BASE_URL}/current.json?key=${KEY}&q=${city}`)
+      const response = await axios.get(`${BASE_URL}/current.json?key=${KEY}&q=${city}`)
       return response.data;
     } catch (error) {
-      console.error("Error Seeing Weather",error);
+      console.error("Error Seeing Weather", error);
       throw error;
     }
 
